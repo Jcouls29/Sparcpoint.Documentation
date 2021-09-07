59765d65d752b806ed6c589f4d847954b6d1514f
Justin Coulston <justin.coulston@gmail.com>
2021-09-06 23:34:32 -0400

    Added baseline logging

--- Sparcpoint.Documentation/Commands/BuildSqlCommand.cs
+++ Sparcpoint.Documentation/Commands/BuildSqlCommand.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Microsoft.SqlServer.TransactSql.ScriptDom;
 using Sparcpoint.Documentation.Abstractions;
 using Sparcpoint.Documentation.Files;
@@ -49,14 +50,20 @@ public static class BuildSqlCommand
             typeof(CreateTypeStatement),
         });
 
-        IServiceProvider provider = BuildDependencyRoot(template, output, noIndexPage);
+        IServiceProvider provider = BuildDependencyRoot(template, output, noIndexPage, verbose);
         var sqlLoaderFactory = provider.GetRequiredService<IFileStructureLoaderFactory<TSqlStatement>>();
         var handler = provider.GetRequiredService<ISqlServerStatementHandler>();
+        var logger = provider.GetRequiredService<ILogger<_BuildSqlCommand>>();
 
         if (!Directory.Exists(output))
+        {
+            logger.LogTrace("Creating directory '{Directory}'...", output);
             Directory.CreateDirectory(output);
+            logger.LogInformation("Created directory '{Directory}'", output);
+        }
 
         IEnumerable<string> files = sqlLoaderFactory.FromPath(path).Load();
+        logger.LogInformation("Loaded {FileCount} SQL Files.", files.Count());
 
         TSql150Parser parser = new TSql150Parser(true);
         SqlScriptGenerator generator = CreateSqlGenerator();
@@ -65,34 +72,87 @@ public static class BuildSqlCommand
         List<TSqlStatement> statements = new List<TSqlStatement>();
         foreach (var file in files)
         {
+            logger.LogTrace("\tSQL File Loaded: {File}", file);
+
             using (var reader = new StreamReader(file))
             {
                 TSqlScript script = (TSqlScript)parser.Parse(reader, out IList<ParseError> errors);
 
                 foreach (var batch in script.Batches)
+                {
+                    if (verbose)
+                    {
+                        generator.GenerateScript(batch, out string batchOutput);
+                        logger.LogTrace("\t\t{Sql}", batchOutput);
+                    }
+
                     statements.AddRange(batch.Statements);
+                }
+                    
             }
         }
 
+        var unhandledStatements = statements
+            .Where(s => !handler.CanHandle(s))
+            .ToArray();
+        
+        foreach(var statementType in unhandledStatements.Select(s => s.GetType()).Distinct())
+        {
+            logger.LogWarning("WARNING: Cannot handle statement of type '{Type}'", statementType.Name);
+        }
+
         var sortedStatements = statements
-            .Where(s => handler.CanHandle(s))
+            .Except(unhandledStatements)
             .OrderBy(s =>
             {
                 return SortedTypes.IndexOf(s.GetType());
             })
             .ToArray();
 
-        foreach (var statement in sortedStatements)
-            handler.Handle(statement, tree, generator);
+        logger.LogInformation("Total Parsed Sql Statements: {SupportedCount}/{TotalCount}", sortedStatements.Length, statements.Count());
+        logger.LogInformation("Beginning Processing of Statements... {Count}", sortedStatements.Length);
 
+        int iStatement = 1;
+        foreach (var statement in sortedStatements)
+        {
+            logger.LogTrace("\tStatement {Index}:", iStatement);
+            if (verbose)
+            {
+                generator.GenerateScript(statement, out string statementOutput);
+                logger.LogTrace("\t\t{Sql}", statementOutput);
+            }
+
+            handler.Handle(statement, tree, generator);
+            iStatement++;
+        }
+
+        var deferredConstraints = tree.GetDeferredConstraints();
+
+        logger.LogInformation("Beginning Processing of Constraints... {Count}", deferredConstraints.Count());
         var constraintHandler = provider.GetRequiredService<ISqlServerConstraintHandler>();
-        foreach (var constraint in tree.GetDeferredConstraints())
+        foreach (var constraint in deferredConstraints)
+        {
             if (constraintHandler.CanHandle(constraint))
+            {
+                if (verbose)
+                {
+                    generator.GenerateScript(constraint.Constraint, out string constraintOutput);
+                    logger.LogTrace("\t{TableName} => {Sql}", constraint.Table?.Identifier, constraintOutput);
+                }
+
                 constraintHandler.Handle(constraint, tree, generator);
+            } else
+            {
+                logger.LogWarning("\tWARNING: Cannot handle constraint of type: {Type}", constraint.Constraint?.GetType()?.Name ?? "Unknown");
+            }
+        }
 
         // NOTE: Tree is built at this point
         ISqlTreeRenderer renderer = provider.GetRequiredService<ISqlTreeRenderer>();
+
+        logger.LogInformation("Beginning Rendering of Sql Models...");
         await renderer.RenderAsync(tree);
+        logger.LogInformation("Build Complete!");
     }
 
     private static SqlScriptGenerator CreateSqlGenerator()
@@ -105,7 +165,7 @@ public static class BuildSqlCommand
         });
     }
 
-    private static IServiceProvider BuildDependencyRoot(string templateDirectory, string outputDirectory, bool noIndexPage)
+    private static IServiceProvider BuildDependencyRoot(string templateDirectory, string outputDirectory, bool noIndexPage, bool verbose = false)
     {
         IServiceCollection services = new ServiceCollection();
         
@@ -114,7 +174,15 @@ public static class BuildSqlCommand
         services.AddStubble();
         services.AddSqlServerHandlers(!noIndexPage);
 
+        services.AddLogging(configure => configure.AddConsole());
+        services.Configure<LoggerFilterOptions>(options =>
+        {
+            options.MinLevel = verbose ? LogLevel.Trace : LogLevel.Information;
+        });
+
         return services.BuildServiceProvider();
     }
+
+    private class _BuildSqlCommand { }
 }
 
